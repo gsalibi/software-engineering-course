117cf943161116347397f41b25505f5c5c88305b
GiordanoMattielli <giordanomatie@hotmail.com>
2018-05-22 21:18:52 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package teste.gui;
+
+public class Main {
+    public static void main(String args[]){
+       Login tela = new Login(); 
+       tela.setVisible(true);
+        
+    }
+    
+    
+}
